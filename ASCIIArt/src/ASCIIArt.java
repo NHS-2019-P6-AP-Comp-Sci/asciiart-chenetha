d1a28e8818c4966e4f960e708daf634935051994
s-chenetha <s-chenetha@53S50030702.it.bsd405.org>
2019-09-20 13:28:05 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -16,12 +16,75 @@
 
 // YOU CODE HERE :) 
 
+
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		antenna();
+		face();
+		body();
+		lowerBody();
+	}
+	public static void face() {
+		// special character /n
+		System.out.println("                  |--------------| ");
+		System.out.println("                  |   --    --   | ");
+		System.out.println("                 _|    |     |   |_ ");
+		System.out.println("                | |              | |");
+		System.out.print("                | |      /\\      | |");
+		System.out.println("\n                | |              | |");
+		System.out.println("                - |    \\____/    |-");
+		System.out.println("                  |--------------| ");
+	}
+	public static void body() {
+		neck();
+		bodyLines();
+		bodyLines();
+		System.out.println("       ____|----------------------------|____");
+		System.out.println("      / - /|----------------------------|\\ - \\");
+		System.out.println("     / - / |----------------------------| \\ - \\");
+		System.out.println("     |  |  |----------------------------|  |  |");
+		System.out.println("     |  |  |----------------------------|  |  |");
+		System.out.println("     |  |  |----------------------------|  |  |");
+		System.out.println("     |  |  |----------------------------|  |  |");
+		System.out.println("     |  |  |----------------------------|  |  |");
+		System.out.println("     |  |  |----------------------------|  |  |");
+		System.out.println("    / \\ /\\ |----------------------------| / \\ /\\");
+		System.out.println("           |____________________________|");
+
+	}
+	public static void lowerBody() {
+		pairOfLegs();
+		pairOfLegs();
+		pairOfLegs();
+		pairOfLegs();
+		pairOfLegs();
+		feet();
+	}
+	public static void antenna() {
+		System.out.println("                          _      ");
+		System.out.println("                         / \\      ");
+		System.out.println("                         |_|        ");
+		System.out.println("                         |_|       ");
+		System.out.println("                         \\_/      ");
+	}
+	public static void neck() {
+		neckLines();
+		neckLines();
+	}
+	public static void neckLines() {
+		System.out.println("                       |----|    ");
+	}
+	public static void bodyLines() {
+		System.out.println("           |----------------------------|");
+	}
+	public static void pairOfLegs() {
+		System.out.println("                |  --  |     |  --  |     ");
+	}
+	public static void feet() {
+		System.out.println("                /------|     |------\\");
+		System.out.println("           -----       |     |       -----");
+        System.out.println("           |___________|     |____________|");
 	}
 }
